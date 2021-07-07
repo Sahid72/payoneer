74d03487b8cc8f50bf9e628a0b6eed49662fe9f3
Sahid72 <67852846+Sahid72@users.noreply.github.com>
2021-07-07 21:23:41 +0600

    javasctipt added

---
+++ script.js
@@ -0,0 +1,27 @@
+// "use strict"
+
+
+const loginBtn = document.getElementById("login");
+loginBtn.addEventListener("click", function(){
+    document.getElementById("login-area").style.display = "none";
+   var transactionArea = document.getElementById("transaction-area");
+   transactionArea.style.display = "block";
+})
+
+
+
+
+
+
+    if(true){
+        var y = "i'm var declaration";
+    }
+    
+
+
+console.log(y);
+
+if(true){
+    let x = "I'm let declartion";
+}
+console.log(x);
