b4e5b6374a5f9f48da9ea9e17c62ca3aee99bbb5
Sahid72 <67852846+Sahid72@users.noreply.github.com>
2021-07-09 12:51:09 +0600

    added  bank deposit and  add

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // "use strict"
 
-
+// login button event handelar 
 const loginBtn = document.getElementById("login");
 loginBtn.addEventListener("click", function(){
     document.getElementById("login-area").style.display = "none";
@@ -10,18 +10,51 @@ loginBtn.addEventListener("click", function(){
 
 
 
+// making fn to update deposit and total balance
+function updateBalance (id,input){
 
-
-
-    if(true){
-        var y = "i'm var declaration";
-    }
-    
-
-
-console.log(y);
-
-if(true){
-    let x = "I'm let declartion";
+    const currentBalance = parseFloat(document.getElementById(id).innerText);
+    // input balance 
+    const inputBalance = parseFloat(document.getElementById(input).value) ;
+    //   calculating total balance 
+    const totalBalance = currentBalance + inputBalance;
+    // pushing balance to right place 
+    document.getElementById(id).innerText = totalBalance;
 }
-console.log(x);
+
+function vanishInput(id){
+document.getElementById(id).value = "";
+}
+// deposit button event handler
+const depositBtn = document.getElementById("addDeposit");
+depositBtn.addEventListener("click", function(event){
+
+        updateBalance("deposit","depositAmount");
+        updateBalance("balanceAmount","depositAmount");
+        vanishInput("depositAmount");
+})
+
+
+// making function to calculate total withdraw and update total balance 
+function withdrawBalance(id, input){
+    const currentBalance = parseFloat(document.getElementById(id).innerText);
+    const currentInput =parseFloat( document.getElementById(input).value );
+    const totalBalance = currentBalance - currentInput;
+    document.getElementById(id).innerText = totalBalance;
+}
+
+// withdraw btn Event handler 
+
+const withdrawBtn = document.getElementById("withdrawbtn");
+withdrawBtn.addEventListener("click", function(){
+
+updateBalance("totalwithdraw","withdrawAmount");
+withdrawBalance("balanceAmount","withdrawAmount")
+    vanishInput("withdrawAmount");
+
+
+})
+
+
+
+  
